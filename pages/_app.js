f9ce93a84f683dda759c857cae3b0d4d523e0b30
Albert Lin <albert.lin@ogglobal.net>
2024-08-27 15:15:27 +0800

    i18n: adjust font family base on language

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,29 @@
 import React, { useEffect } from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import store from "../redux/store";
-import { appWithTranslation } from "next-i18next";
+import { appWithTranslation, useTranslation } from "next-i18next";
 import "../styles/globals.scss";
 import "../styles/tailwindcss.css";
-import { useLanguageSwitch } from '$UTILS/lang/useLanguageSwitch'
+import { useLanguageSwitch } from "$UTILS/lang/useLanguageSwitch";
 
 function MyApp({ Component, pageProps }) {
-    const { status, text } = useSelector((state) => state.spin);
+    const { i18n } = useTranslation();
 
     console.log("Redux Store State:", store.getState());
     useLanguageSwitch();
-    
+
     useEffect(() => {
         window.piwikLoadFinished = true;
     }, []);
+
+    //根據不同語言設定字體和 HTML lang 屬性
+    useEffect(() => {
+        // 設置 HTML lang 屬性
+        document.documentElement.lang = i18n.language;
+        // 設置字體類
+        document.body.className =
+            i18n.language === "hi" ? "font-hind" : "font-roboto";
+    }, [i18n.language]);
 
     return <Component {...pageProps} />;
 }
